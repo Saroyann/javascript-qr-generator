864909f7ec2263b10e1bd51cfe792a00a59cc051
Yiim <ilyimyan@gmail.com>
2024-07-10 23:12:04 +0800

    initial commit

--- script.js
+++ script.js
@@ -7,6 +7,6 @@ const generateQR = () => {
 qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText.value}`
 }
 
-button.addEventListener(onclick, () => {
+button.addEventListener('click', () => {
     return generateQR();
 })
