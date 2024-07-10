3afc3dd5a9f9ba3601f88b65043bbfbf9133368d
Yiim <ilyimyan@gmail.com>
2024-07-10 23:17:52 +0800

    initial commit

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const qrText = document.querySelector("#qrText");
 const button = document.querySelector("#button");
 
 const generateQR = () => {
-qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText.value}`
+qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText.value}`;
+
+imgBox.classList.add("show-img")
 }
 
 button.addEventListener('click', () => {
