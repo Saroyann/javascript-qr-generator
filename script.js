8c4b323dfc7799504c7594d6c3cda156e1752b45
Yiim <ilyimyan@gmail.com>
2024-07-10 23:10:39 +0800

    initial commit

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-let imgBox = document.querySelector("#imgBox");
-let qrImage = document.querySelector("#qrImage");
-let qrText = document.querySelector("#qrText");
+const imgBox = document.querySelector("#imgBox");
+const qrImage = document.querySelector("#qrImage");
+const qrText = document.querySelector("#qrText");
+const button = document.querySelector("#button");
 
 const generateQR = () => {
-qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText}`
+qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText.value}`
 }
+
+button.addEventListener(onclick, () => {
+    return generateQR();
+})
