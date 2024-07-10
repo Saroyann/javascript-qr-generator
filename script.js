669c7c7b1284975837c66a3469afd6f7bd1a7a45
Yiim <ilyimyan@gmail.com>
2024-07-10 23:05:52 +0800

    initial commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let imgBox = document.querySelector("#imgBox");
 let qrImage = document.querySelector("#qrImage");
-
+let qrText = document.querySelector("#qrText");
 
 const generateQR = () => {
-
+qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example"
 }
